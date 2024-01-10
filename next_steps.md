b50dfa1f97fc666e6ce577ea255b9228a9e69a07
Daniel Genkins <64920363+d-genk@users.noreply.github.com>
2024-01-10 16:56:33 -0600

    modular few-shot training and relationship extraction

--- next_steps.md
+++ next_steps.md
@@ -1,9 +1,12 @@
-1. add ID generation for people
-2. add relationship construction
-3. add place and event extraction
+3. add event extraction
 4. test different books
 5. test different types of ecclesiastical records
-6. load examples from outside the script
 7. "fine-tune" few-shot learning for different types of records/languages
 8. load comprehensive controlled vocabularies from outside the script
 9. build a service
+10. add infrastructure to iterate across volume, fill in missing dates, disambiguate, etc.
+
+Other possibilities:
+-assign unique identifiers for places
+    -pros: slightly easier to link data
+    -cons: adds mostly unnecessary complexity to model instructions
--- sequential.py
+++ sequential.py
@@ -1,25 +1,50 @@
 from openai import OpenAI
 client = OpenAI()
 
-sample_transcription = "En diezynuebe de Julio de mil ochocientoscuarenta; yo Don Antonio Loreto Sanchez Presbitero cura beneficiado por " \
+example_01 = "En diezynuebe de Julio de mil ochocientoscuarenta; yo Don Antonio Loreto Sanchez Presbitero cura beneficiado por " \
                 "Su Majestad de la Iglesia Parroquial de la Purisima Concepción de esta villa de Cienfuegos en ella y su jurisdiccion vicario Eclesiastico por " \
                     "Santa Eclesiastica Illustrisima bauticé solemnemente y puse los santos oleos a una negra Carabalí adulta natural de Africa, de la propiedad de Capitán Juan Vives, " \
                         "en cuya negra ejerci las sacras ceremonias y preces y la puse por nombre Dominga fue su madrina Isabel aquien adverti el parentesco espiritual y demas " \
                             "obligaciones y lo firmé"
 
-sample_transcription_2 = "En diezyseis de Octubre de mil ochocientostreintaynuebe; yo Don Antonio Loreto Sanchez Presbitero cura beneficiado " \
+output_01 = ""
+
+with open("example_01.json","r",encoding="utf-8") as f:
+    for line in f:
+        output_01 += line
+
+example_02 = "En diezyseis de Octubre de mil ochocientostreintaynuebe; yo Don Antonio Loreto Sanchez Presbitero cura beneficiado " \
                          "por Santa Majestad de la Iglesia Parroquial de la Purisima Concepción de esta villa de Cienfuegos en ella y su jurisdiccion " \
-                         "vicario Eclesiastico Santa Eclesiastica Illustrisima bautice solemnemente y puse los santos oleos a una parvula Española que nació el trece de Junio " \
+                         "vicario Eclesiastico Santa Eclesiastica Illustrisima bautice solemnemente y puse los santos oleos a una parvula que nació el trece de Junio " \
                          "del año de mil ochocientostreintaysiete hija legitima de los morenos Julian y Sabina, naturales de Guinea esclavos " \
                          "de Don Jose Corras: en cuya parvula ejervi las sacras ceremonias y preces y le puse por nombre Antonia " \
                          "fue su madrina Budesiceda aquien adverti el parentesco espiritual y demas obligaciones y lo firme"
 
-transcription = "En diezyseis de Octubre de mil ochocientostreintaynuebe; yo Don Juan Gomes Presbitero cura beneficiado por Su Majestad de " \
+output_02 = ""
+
+with open("example_02.json","r",encoding="utf-8") as f:
+    for line in f:
+        output_02 += line
+
+example_03 = "En diezyseis de Octubre de mil ochocientostreintaynuebe; yo Don Juan Gomes Presbitero cura beneficiado por Su Majestad de " \
                 "la Iglesia Parroquial de la Purisima Concepción de esta villa de Cienfuegos en ella y su jurisdiccion vicario Eclesiastico por Santa Eclesiastica Illustrisima bautice " \
-                "solemnemente y puse los santos oleos a una parvula Gangá que nacio el veinteycuatro de Septiembre del año pasado hija legítima de " \
-                "los negros Sebastian y Jucina naturales de Guinea de la propiedad de Teniente Jose Corras; en cuya parvula libre ejerci las " \
+                "solemnemente y puse los santos oleos a una parvula que nacio el veinteycuatro de Septiembre del año pasado hija de " \
+                "la negra Jucina de la propiedad de Teniente Jose Corras; en cuya parvula libre ejerci las " \
                 "sacras preces y ceremonias y preces y le puse por nombre Merced fue su madrina la parda Doña Maria Ramona Prieto; " \
                 "aquien adverti el parentesco espiritual y demas obligaciones y lo firme"
+
+output_03 = ""
+
+with open("example_03.json","r",encoding="utf-8") as f:
+    for line in f:
+        output_03 += line
+
+user_input = "En diezysies de Octubre de mil ochocientostreintaynuebe; yo Don Antonio Loreto Sanchez Presbitero cura beneficiado por " \
+             "Su Majestad de la Iglesia Parroquial de la Purisima Concepción de esta villa de Cienfuegos en ella y su jurisdiccion vicario " \
+             "Eclesiastico por Santa Eclesiastica Illustrisima bautice solemnemente y puse los santos oleos a una parvula que nacio, el quince de Julio " \
+             "del presente año hija legitima de los morenos Fabian y de Isabel naturales de Africa de la propiedad de Don Jose " \
+             "Corras; en cuya parvula ejerci las sacras ceremonias y preces y le puse por nombre Maria del Carmen; " \
+             "fue su madrina Francisca Maria Herrera; aquien adverti el parentesco espiritual y demas obligaciones y lo firme"
 
 response = client.chat.completions.create(
     model="gpt-3.5-turbo-1106",
@@ -28,7 +53,12 @@ response = client.chat.completions.create(
         {
             "role": "system",
             "content": "You are assisting a historian of the early modern Atlantic with a large collection of transcriptions of Catholic sacramental records written in Spanish. " \
-            "The historian will provide you with a transcription of a sacramental record and ask you to list each person who appears in that transcription. Output this list as JSON. "
+            "The historian will provide you with a transcription of a sacramental record and ask you to extract detailed information from that record. Output this information as JSON. "
+        },
+        {
+            "role": "system",        
+            "content": "These transcriptions contain information about people. You will create a record for each person, containing at minimum their name as it appears in the transcription. " \
+            "Assign each person a unique identifier. These should take the form P##, where ## is a two-digit padded integer and P01 is assigned to the first person to appear in the transcription."            
         },
         {
             "role": "system",
@@ -38,7 +68,8 @@ response = client.chat.completions.create(
         },             
         {
             "role": "system",
-            "content": "These records will sometimes contain information about the places of origin of these people. When they do, please include this information in your output for the person in question."
+            "content": "These records will sometimes contain information about the places of origin of these people. When they do, please include this information in your output for the person in question." \
+            "In Spanish, the presence of this information will often be signalled by words such as `natural` for a single individual or `naturales` for multiple."
         },
         {
             "role": "system",
@@ -72,28 +103,48 @@ response = client.chat.completions.create(
             "content": "These records will sometimes contain information about the freedom status of these people. When they do, please include this information in your output for the person in question. " \
             "In Spanish, this information might appear explicitly as either esclavo/a (list as `enslaved`) or libre (list as `free`). " \
             "Freedom status can also be communicated implicitly, most notably when the enslaver of one or more individuals is listed."
-        },        
+        },
+        {
+            "role": "system",
+            "content": "These records will sometimes contain information about relationships between people. " \
+            "Each relationship that you identify should be between 2 people and appear twice in your output, once for each related person. " \
+            "The relationship type that you list for each individual should indicate what the related person *is to them*."
+        },               
         {            
             "role": "user",
-            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + sample_transcription + "`"
+            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + example_01 + "`"
         },
         {
             "role": "assistant",
-            "content": '{"people":[{"name":"Antonio Loreto Sanchez","titles":["Don"],"occupation":"cleric"},{"name":"Juan Vives","rank":"Captain"},{"name":"Dominga","origin":"Africa","ethnicitiy":"Carabalí","age":"adult","phenotype":"negro","status":"enslaved"},{"name":"Isabel"}]}'
+            "content": output_01
         },
         {            
             "role": "user",
-            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + sample_transcription_2 + "`"
+            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + example_02 + "`"
         },
         {
             "role": "assistant",
-            "content": '{"people":[{"name":"Antonio Loreto Sanchez","titles":["Don"],"occupation":"cleric"},{"name":"Julian","origin":"Guinea","phenotype":"moreno","status":"enslaved"},{"name":"Sabina","origin":"Guinea","phenotype":"moreno","status":"enslaved"},{"name":"Jose Corras","titles":["Don"]},{"name":"Antonia","ethnicity":"Española","age":"infant","legitimacy":"legitimate","status":"free"},{"name":"Budesiceda"}]}'
+            "content": output_02
         },
         {            
             "role": "user",
-            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + transcription + "`"
+            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + example_03 + "`"
+        },
+        {
+            "role": "assistant",
+            "content": output_03
+        },
+        {            
+            "role": "user",
+            "content": "Please list each person who appears in this transcription of a Spanish baptismal register: `" + user_input + "`"
         }
     ]
 )
 
-print(response.choices[0].message.content)
+model_output = response.choices[0].message.content
+
+with open("output.json", "w", encoding="utf-8") as f:
+    for line in model_output:
+        f.write(line)
+
+print("Output saved.")
