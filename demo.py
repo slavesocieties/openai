f37faa9da9fb08366a9006a43cf942ed410c45e8
Daniel Genkins <64920363+d-genk@users.noreply.github.com>
2024-11-11 16:24:57 -0600

    demonstrating how branches work

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ import json
 
 # segment lines from an entry
 
-# open image with PIL
+'''# open image with PIL
 img = Image.open('images/239746-0044-01.jpg').convert('L')
 
 # rotate image for straightest possible lines
@@ -18,7 +18,7 @@ crop_pixels = find_pixels(img, 5000)
 
 # save the resulting segmented lines
 entry_segments = segment_lines(img, crop_pixels, 'images/239746-0044-01.jpg', 1, save_dir='demo')
-
+'''
 # identify coordinates of individual entries in an image
-# with open("segmentation_test.json", "w") as f:
-    # json.dump(segmentation_driver("images/239746-0088.jpg", display=True), f)
+with open("segmentation_test.json", "w") as f:
+    json.dump(segmentation_driver("images/239746-0088.jpg", display=True), f)
