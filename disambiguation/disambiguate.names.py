e42fb0f504edb75d831e6f49cb3298110726c683
caitkyf <ymfcait@hotmail.com>
2025-02-06 15:47:54 -0600

    Update disambiguate.names.py

--- disambiguation/disambiguate.names.py
+++ disambiguation/disambiguate.names.py
@@ -2,7 +2,50 @@ import random
 import json
 import os.path
 
-def read_names(spanish_names):
+
+# create person record
+# id, name, title, occupation, relationships etc.
+# most likely: phenotype, title, maybe ethnicity/free/legitimate, everything else rare, relationships!!
+
+# general function to create field (whatever it is and select from the associated pool)
+# basic control vocab for fields
+# function to generate relationships
+# update record creation function to build full record
+
+# create single term
+# personRecords is existing dictionary, add termToCreate
+# can also modify: create personRecords within method and return single term dict
+
+allTerms = ["f_names"]
+
+def createTerm(personRecords, termToCreate):
+    randomTerm = "N/A"
+    currentDir = os.getcwd()
+    parentDir = os.path.dirname(currentDir)
+    pathToFile = os.path.join(parentDir, "vocab.json")
+    with open(pathToFile, 'r') as allVocab:
+        data = json.load(allVocab)
+        allVocab = data["controlled_vocabularies"]
+    for vocabSet in allVocab:
+        if vocabSet["key"] == termToCreate:
+            vocabIndex = random.randint(0, len(vocabSet["vocab"]) - 1)
+            randomTerm = vocabSet["vocab"][vocabIndex]
+            break
+    personRecords[termToCreate] = randomTerm
+
+
+# Test code
+'''personRecords = {}
+createTerm(personRecords, "titles")
+print(personRecords)'''
+
+
+
+
+
+# here lies scrap code RIP
+
+'''def read_names(spanish_names):
     with open(spanish_names, 'r') as sn:
         data = json.load(sn)
         names = data['names']
@@ -36,10 +79,7 @@ def generateName():
     gen_name = random.choice(fname) + ' ' + random.choice(lname)
     return gen_name
 
-# create person record
-# id, name, title, occupation, relationships etc.
-# most likely: phenotype, title, maybe ethnicity/free/legitimate, everything else rare, relationships!!
-'''def generatePersonRecord():
+def generatePersonRecord():
     indivRecord = {}
     indivRecord['name'] = generateName()
     return indivRecord
@@ -53,14 +93,9 @@ def addPersonRecord(peopleList):
 def testMyCode():
     myDictionary = {}
     addPersonRecord(myDictionary)
-    print(myDictionary)'''
+    print(myDictionary)
 
-# general function to create field (whatever it is and select from the associated pool)
-# basic control vocab for fields
-# function to generate relationships
-# update record creation function to build full record
-
-'''def createPool():
+def createPool():
     vocabPool = {}
     currentDir = os.getcwd()
     parentDir = os.path.dirname(currentDir)
@@ -70,37 +105,12 @@ def testMyCode():
         allVocab = data["controlled_vocabularies"]
         for vocabSet in allVocab:
             vocabPool[vocabSet["key"]] = vocabSet["vocab"]
-    return vocabPool'''
+    return vocabPool
 
-
-# create single term
-# personRecords is existing dictionary, add termToCreate
-# can also modify: create personRecords within method and return single term dict
-def createTerm(personRecords, termToCreate):
-    randomTerm = "N/A"
-    currentDir = os.getcwd()
-    parentDir = os.path.dirname(currentDir)
-    pathToFile = os.path.join(parentDir, "vocab.json")
-    with open(pathToFile, 'r') as allVocab:
-        data = json.load(allVocab)
-        allVocab = data["controlled_vocabularies"]
-    for vocabSet in allVocab:
-        if vocabSet["key"] == termToCreate:
-            vocabIndex = random.randint(0, len(vocabSet["vocab"]) - 1)
-            randomTerm = vocabSet["vocab"][vocabIndex]
-            break
-    personRecords[termToCreate] = randomTerm
-
-
-'''def createNewPerson(vocabPool):
+def createNewPerson(vocabPool):
     personRecords = {}
     personRecords["name"] = generateName()
     for vocabType, vocabList in vocabPool.items():
         vocabIndex = random.randint(0, len(vocabList) - 1)
         personRecords[vocabType] = vocabList[vocabIndex]
     return personRecords'''
-
-# Test code
-'''personRecords = {}
-createTerm(personRecords, "titles")
-print(personRecords)'''
